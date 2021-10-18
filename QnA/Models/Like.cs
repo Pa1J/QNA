6b2ca0645a96fc4ae9ac0f04c5811627847bd31e
Pa1J <itspa1j24@gmail.com>
2021-10-18 18:19:46 +0530

    first commit

---
+++ QnA/Models/Like.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QnA.Models
+{
+    public class Like
+    {
+        public int Id { get; set; }
+        public int LikedBy { get; set; }
+        public int AnswerId { get; set; }
+        public bool IsLiked { get; set; }
+        public DateTime LikedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
---
+++ QnA/Models/Vote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QnA.Models
+{
+    public class Vote
+    {
+        public int Id { get; set; }
+        public int QuestionId { get; set; }
+        public int UserId { get; set; }
+        public DateTime VotedOn { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
